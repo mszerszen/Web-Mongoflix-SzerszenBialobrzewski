069686366b2b48626625a32fe3dde63e41f453f5
IgnacyBialobrzewski <ignacy.bialobrzewski@cyfroweszkoly.pl>
2026-02-25 09:12:11 +0100

    prepare analysis script for queries

--- queries/analysis.mongodb.js
+++ queries/analysis.mongodb.js
@@ -1,8 +1,11 @@
 import { MongoClient } from "mongodb"
 import { argv } from "node:process"
 
-const [ _, __, host, port ] = argv
-const url = `mongodb://${host ?? "localhost"}:${port ?? "27017"}`
+const [ _, __, database, collection, host, port ] = argv
+const url = `mongodb://${host ?? "localhost"}:${port ?? "27017"}/${database}`
+
+assert(database, "database wasn't provided as the first argument in argv")
+assert(collection, "collection wasn't provided as the second argument in argv")
 
 const client = new MongoClient(url)
 
