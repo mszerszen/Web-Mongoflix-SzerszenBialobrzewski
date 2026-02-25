5f8ceb24fc7724ce8be7a20a80508a8badcd3207
IgnacyBialobrzewski <ignacy.bialobrzewski@cyfroweszkoly.pl>
2026-02-25 08:36:45 +0100

    establish connection with mongo

--- queries/analysis.mongodb.js
+++ queries/analysis.mongodb.js
@@ -1,1 +1,20 @@
+import { MongoClient } from "mongodb"
+import { argv } from "node:process"
 
+const [ _, __, host, port ] = argv
+const url = `mongodb://${host ?? "localhost"}:${port ?? "27017"}`
+
+const client = new MongoClient(url)
+
+console.log(url)
+
+async function main() {
+    try {
+        await client.connect()
+        console.log("connect OK")
+    } finally {
+        await client.close()
+    }
+}
+
+main().catch(console.log)
