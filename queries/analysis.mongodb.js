e913cdd6c180f26d48e711bf83c354c00218ec7e
Marceli Szerszeń <marceliszerszen08@gmail.com>
2026-03-03 14:14:04 +0100

    Print Queries

--- queries/analysis.mongodb.js
+++ queries/analysis.mongodb.js
@@ -1,19 +1,19 @@
 db = db.getSiblingDB('filmy_bs');
 
-const genre = "Sci-Fi";
+const genre = "Action";
 const year = 2008;
 
 print("SZUKANIE PO GATUNKU")
 db.filmy.find(
     { genres: genre },
     { title: 1, genres: 1, _id: 0 }
-);
+).forEach(doc => printjson(doc))
 
 print("\nFILTROWANIE PO ROKU")
 db.filmy.find(
-    { releaseYear: genre },
+    { releaseYear: year },
     { title: 1, genres: 1, _id: 0 }
-);
+).forEach(doc => printjson(doc))
 
 print("\nŚREDNIA OCEN DLA KAŻDEGO GATUNKU")
 db.filmy.aggregate([
@@ -32,7 +32,7 @@ db.filmy.aggregate([
             averageRating: { $round: ["$averageRating", 2] }
         }
     }
-]);
+]).forEach(doc => printjson(doc))
 
 print("\nDODANIE FILMÓW FEATURED")
 db.filmy.updateMany(
@@ -40,7 +40,7 @@ db.filmy.updateMany(
     { $set: { featured: true } }
 );
 
-print("\nLICZBA WYŚWIETLEŃ DLA REŻYSERÓw ")
+print("\nLICZBA WYŚWIETLEŃ DLA REŻYSERÓW ")
 db.filmy.aggregate([
     {
         $group: {
